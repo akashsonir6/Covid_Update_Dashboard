6fa3ca0859b9031e42828fc2fe44cd84860d6bae
Akash Soni <akashkumarsonir6@gmail.com>
2021-05-18 00:17:10 +0530

    Update Countries.jsx

--- covid-19/src/components/Countries.jsx
+++ covid-19/src/components/Countries.jsx
@@ -17,8 +17,8 @@ const Countries = ({ handleCountryChange }) => {
 
     return (
         <>
-            <Typography style={{ marginBottom: 20 }, { fontSize: '3vw' }} variant="h4" color="textSecondary">
-                Reported Cases or Deaths by countries or Territoty
+            <Typography style={{ marginBottom: 20, fontSize: '3vw' }} variant="h4" color="textSecondary">
+                Reported Cases or Deaths by Countries or Territoty
         </Typography>
             <NativeSelect onChange={(e) => handleCountryChange(e.target.value)}>
                 <option value="" > Worldwide</option>
